c71f8748e285152f8401f9a88b206d276e11d092
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2012-11-09 02:27:14 +0000

    Small changes. Avoiding abstractifying the Global class - it really doesn't need to be.
    - Removed Global.getSystemTimeMillis() as System.currentTimeMillis() is just as effective. getSystemTimeMillis() was intended to be a temporary bridge between Eclipse and Netbeans; as such, it is no longer needed.
    - Played around with making execute() a protected function; deemed it wasn't necessary
    - Significantly cleaned Global.java. Added Deprecated Events.txt which holds events that have outlived their lifespans. Added Port Data.txt to hold port information which has either expired or is not needed - THESE FILES NEED REMAIN DATESTAMPED
    - Changed ((Event) ListenerManager.event.getElementAt(i)).execute() to ((Event)....At(i)).registerEvent(); to improve containment policies. This is to counter the second item in the list.
    - Added robotReset to reset the event tables quickly and easily. Placed these in all init functions
    - Added robotKill to quickly kill all robot events
    - Added Watchdog.getInstance().feed() to all periodic functions. Should help.

--- trunk/FRC-3128-2013/src/edu/wpi/first/wpilibj/templates/Global.java
+++ trunk/FRC-3128-2013/src/edu/wpi/first/wpilibj/templates/Global.java
@@ -2,7 +2,6 @@ package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.templates.Controller.XControl;
-import edu.wpi.first.wpilibj.templates.Controller.XControlMap;
 import edu.wpi.first.wpilibj.templates.EventManager.Event;
 import edu.wpi.first.wpilibj.templates.EventManager.EventManager;
 import edu.wpi.first.wpilibj.templates.ListenerManager.ListenerManager;
@@ -13,72 +12,37 @@ class WatchdogEvent extends Event {
     }
 }
 
-
-/*class DriveTank extends Event {
-    public void execute() {
-        double x = Global.joy.getRawAxis(1)/1.25;
-        double y = -Global.joy.getRawAxis(2)/1.25;
-        
-        if (Math.abs(y) < 0.15) y = 0;
-        if (Math.abs(x) < 0.15) x = 0;
-        
-        DebugLog.log(4, referenceName, "Y: "+y+", "+x);
-        Global.mLB.set(((y-(x/1.5))/1.3));
-        Global.mRB.set((-(y+(x/1.5))));
-        Global.mLF.set(((y-(x/1.5))/1.3));
-        Global.mRF.set((-(y+(x/1.5))));
-    }
-}
-
-class CatapultOn extends Event {
-    public void execute() {
-        if (Global.joy.getRawButton(XControlMap.Y)) {
-            Global.catapultOne.set(true);
-            Global.catapultTwo.set(false);
-        }
-    }
-}
-
-class CatapultOff extends Event {
-    public void execute() {
-        if (Global.joy.getRawButton(XControlMap.A)) {
-            Global.catapultOne.set(false);
-            Global.catapultTwo.set(true);
-        }
-    }
-}*/
-
 public class Global {
     public final static String referenceName = "Global";
     public static boolean shutdown = false;
-    public final static EventManager eventManager = new EventManager();
-    //public final static Jaguar mLB = new Jaguar(1, 3);
-    //public final static Jaguar mRB = new Jaguar(1, 6);
-    //public final static Jaguar mLF = new Jaguar(1, 2); //Works
-    //public final static Jaguar mRF = new Jaguar(1, 1); //Works
-    //public final static Jaguar mBAD1 = new Jaguar(1, 9);
-    //public final static Jaguar mBAD2 = new Jaguar(1, 4); //Works
-    //public final static Joystick joy = new Joystick(1);
- 
-    //public final static Compressor comp = new Compressor(1, 1, 1, 1);
-    //public final static Solenoid catapultOne = new Solenoid(1, 3);
-    //public final static Solenoid catapultTwo = new Solenoid(1, 4);
-   
+    public final static EventManager eventManager = new EventManager();    
     public static XControl xControl1 = new XControl(1);
-
-    public static long getSystemTimeMillis() {
-        return System.currentTimeMillis();
-    }
 
     public static void initializeRobot() {
         DebugLog.setLogLevel(4);
+        (new WatchdogEvent()).registerIterable();
     }
 
+    public static void initializeDisabled() {
+        
+    }
+    
     public static void initializeAuto() {
     
     }
 
     public static void initializeTeleop() {
         
+    }
+    
+    public static void robotReset() {
+        EventManager.dropEvents();
+        ListenerManager.dropListeners();
+    }
+    
+    public static void robotKill() {
+        EventManager.dropEvents();
+        ListenerManager.dropListeners();
+        Watchdog.getInstance().kill();
     }
 }
--- trunk/FRC-3128-2013/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ trunk/FRC-3128-2013/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,6 +1,7 @@
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Watchdog;
 import edu.wpi.first.wpilibj.templates.EventManager.EventManager;
 
 public class RobotTemplate extends IterativeRobot {
@@ -9,11 +10,12 @@ public class RobotTemplate extends IterativeRobot {
     }
     
     public void disabledInit() {
-        
+        Global.robotReset();
+        Global.initializeDisabled();
     }
     
     public void disabledPeriodic() {
-        
+        Watchdog.getInstance().feed();
     }
     
     public void disabledContinuous() {
@@ -21,11 +23,12 @@ public class RobotTemplate extends IterativeRobot {
     }
     
     public void autonomousInit() {
+        Global.robotReset();
         Global.initializeAuto();
     }
     
     public void autonomousPeriodic() {
-
+        Watchdog.getInstance().feed();
     }
     
     public void autonomousContinuous() {
@@ -33,11 +36,12 @@ public class RobotTemplate extends IterativeRobot {
     }
     
     public void teleopInit() {
+        Global.robotReset();
         Global.initializeTeleop();
     }
     
     public void teleopPeriodic() {
-        
+        Watchdog.getInstance().feed();        
     }
     
     public void teleopContinuous() {
