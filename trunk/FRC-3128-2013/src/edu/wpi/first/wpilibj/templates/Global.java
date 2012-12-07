08d1826001bbe9cc72a4fcad4f787b877bd3e932
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2012-12-07 00:25:42 +0000

    Few changes, code running. Tank, nothing fancy.

--- trunk/FRC-3128-2013/src/edu/wpi/first/wpilibj/templates/Global.java
+++ trunk/FRC-3128-2013/src/edu/wpi/first/wpilibj/templates/Global.java
@@ -12,15 +12,36 @@ class WatchdogEvent extends Event {
     }
 }
 
+class DriveTank extends Event {
+    public void execute() {
+        double x = Global.xControl1.x1;
+        double y = Global.xControl1.y1;
+        
+        if (Math.abs(y) < 0.15) y = 0;
+        if (Math.abs(x) < 0.15) x = 0;
+        
+        DebugLog.log(4, referenceName, "Y: "+y+", "+x);
+        Global.mLB.set(((y-(x/1.5))/1.3));
+        Global.mRB.set((-(y+(x/1.5))));
+        Global.mLF.set(((y-(x/1.5))/1.3));
+        Global.mRF.set((-(y+(x/1.5))));
+    }
+}
+
 public class Global {
     public final static String referenceName = "Global";
     public static boolean shutdown = false;
-    public final static EventManager eventManager = new EventManager();    
+    public final static EventManager eventManager = new EventManager();
+    public final static Jaguar mLB = new Jaguar(1, 1);
+    public final static Jaguar mRB = new Jaguar(1, 4);
+    public final static Jaguar mLF = new Jaguar(1, 2);
+    public final static Jaguar mRF = new Jaguar(1, 3);
     public static XControl xControl1 = new XControl(1);
 
     public static void initializeRobot() {
         DebugLog.setLogLevel(4);
         (new WatchdogEvent()).registerIterableEvent();
+        ListenerManager.addListener((new DriveTank()), "updateJoy1");
     }
 
     public static void initializeDisabled() {
