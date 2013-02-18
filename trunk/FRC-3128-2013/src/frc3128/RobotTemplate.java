3f01517a092fc6d50da23719c97b5e07b5f605fb
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-02-18 06:15:51 +0000

    First, changed Global events to be processed when appropriate. This is logical, but knowing some of the FIRST API stuff, it'll have to be changed. Nonetheless, the jump between Autonomous and Teleop should now be as simple as dropping all events and listeners, then reinstantiating the necessary ones with initializers.
    Second, did a lot of reorganizing. Grouped the declarations in Global by relevance to each other, then organized the functions by relevance to order of operations. Regrouped and cleaned up the RobotTemplate functions as well.

--- trunk/FRC-3128-2013/src/frc3128/RobotTemplate.java
+++ trunk/FRC-3128-2013/src/frc3128/RobotTemplate.java
@@ -1,41 +1,34 @@
 package frc3128;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Watchdog;
 import frc3128.EventManager.EventManager;
-import frc3128.ListenerManager.ListenerManager;
 
 public class RobotTemplate extends IterativeRobot {
     public void robotInit() {
         Global.initializeRobot();
     }
-   
-    public void disabledInit() {
-        Global.initializeDisabled();
-    }
-    
-    public void disabledPeriodic() {        
-        Watchdog.getInstance().feed();
-        EventManager.processEvents();
-    }
-    
-    public void autonomousInit() {
-        Global.robotReset();
-        Global.initializeAuto();
-    }
-    
-    public void autonomousPeriodic() {
-        Watchdog.getInstance().feed();
-        EventManager.processEvents();
-    }
-    
+
     public void teleopInit() {
         Global.initializeTeleop();
     }
     
+    public void autonomousInit() {
+        Global.initializeAuto();
+    }
+    
+    public void disabledInit() {
+        Global.initializeDisabled();
+    }
+
     public void teleopPeriodic() {
-        //System.out.println(System.currentTimeMillis());
-        Watchdog.getInstance().feed();
+        EventManager.processEvents();
+    }
+   
+    public void autonomousPeriodic() {
+        EventManager.processEvents();
+    }
+    
+    public void disabledPeriodic() {        
         EventManager.processEvents();
     }
 }
