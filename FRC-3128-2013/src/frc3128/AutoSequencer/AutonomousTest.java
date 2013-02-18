62d8793a6cfefb832d99b42dfd4488754ad5ae3a
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-02-18 05:55:46 +0000

    Deleted the TiltControl package because it's a bit too localized. Modified the AutoEvent to be public because I was being stupid. Added the AutonomousTest class.

---
+++ FRC-3128-2013/src/frc3128/AutoSequencer/AutonomousTest.java
@@ -0,0 +1,44 @@
+package frc3128.AutoSequencer;
+
+class AutoEvent1 extends AutoEvent {
+    public boolean exitConditionMet() {
+        if (this.getRunTimeMillis() > 1000) return true;
+        return false;
+    }
+
+    public void execute() {
+        System.out.println("Hello from 1");
+    }
+}
+
+
+class AutoEvent2 extends AutoEvent {
+    public boolean exitConditionMet() {
+        if(this.getRunTimeMillis() > 1000) return true;
+        return false;
+    }
+
+    public void execute() {
+        System.out.println("Hello from 2");
+    }
+}
+
+class AutoEvent3 extends AutoEvent {
+    public boolean exitConditionMet() {
+        return true;
+    }
+    
+    public void execute() {
+        System.out.println("Hello from 3");
+    }
+}
+
+public class AutonomousTest {
+    public AutonomousTest() {
+        AutoSequencer as = new AutoSequencer();
+        as.addEvent((new AutoEvent1()));
+        as.addEvent((new AutoEvent2()));
+        as.addEvent((new AutoEvent3()));
+        as.registerIterableEvent();
+    }
+}
