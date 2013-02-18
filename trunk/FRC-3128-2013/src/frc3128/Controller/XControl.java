a0d0e59ed789ccdeef6dc8c10086e9dc05050d58
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-02-18 06:32:08 +0000

    Removed DriveMecanum for code consistency. Cleaned up an unnecessary try/catch in XControl.

--- trunk/FRC-3128-2013/src/frc3128/Controller/XControl.java
+++ trunk/FRC-3128-2013/src/frc3128/Controller/XControl.java
@@ -17,37 +17,32 @@ public class XControl extends Event {
     }
     
     public void execute() {
-        try {
-            boolean updateJoy1 = false;
-            boolean updateJoy2 = false;
-            boolean updateTriggers = false;
+        boolean updateJoy1 = false;
+        boolean updateJoy2 = false;
+        boolean updateTriggers = false;
 
-            if(x1 != xControl.getRawAxis(XControlMap.x1Axis))       updateJoy1 = true;
-            if(x2 != xControl.getRawAxis(XControlMap.x2Axis))       updateJoy2 = true;
-            if(y1 != xControl.getRawAxis(XControlMap.y1Axis))       updateJoy1 = true;
-            if(y2 != xControl.getRawAxis(XControlMap.y2Axis))       updateJoy2 = true;
-            if(triggers != xControl.getRawAxis(XControlMap.trAxis)) updateTriggers = true;
+        if(x1 != xControl.getRawAxis(XControlMap.x1Axis))       updateJoy1 = true;
+        if(x2 != xControl.getRawAxis(XControlMap.x2Axis))       updateJoy2 = true;
+        if(y1 != xControl.getRawAxis(XControlMap.y1Axis))       updateJoy1 = true;
+        if(y2 != xControl.getRawAxis(XControlMap.y2Axis))       updateJoy2 = true;
+        if(triggers != xControl.getRawAxis(XControlMap.trAxis)) updateTriggers = true;
 
-            x1 = xControl.getRawAxis(XControlMap.x1Axis);
-            x2 = xControl.getRawAxis(XControlMap.x2Axis);
-            y1 = xControl.getRawAxis(XControlMap.y1Axis);
-            y2 = xControl.getRawAxis(XControlMap.y2Axis);
+        x1 = xControl.getRawAxis(XControlMap.x1Axis);
+        x2 = xControl.getRawAxis(XControlMap.x2Axis);
+        y1 = xControl.getRawAxis(XControlMap.y1Axis);
+        y2 = xControl.getRawAxis(XControlMap.y2Axis);
 
-            if(updateJoy1) ListenerManager.callListener("updateJoy1");
-            if(updateJoy2) ListenerManager.callListener("updateJoy2");
-            if(updateTriggers) ListenerManager.callListener("updateTriggers");
-            if(updateJoy1 || updateJoy2 || updateTriggers) ListenerManager.callListener("updateDrive");
+        if(updateJoy1) ListenerManager.callListener("updateJoy1");
+        if(updateJoy2) ListenerManager.callListener("updateJoy2");
+        if(updateTriggers) ListenerManager.callListener("updateTriggers");
+        if(updateJoy1 || updateJoy2 || updateTriggers) ListenerManager.callListener("updateDrive");
 
-            for(int i = 1; i < 11; i++) {
-                if(buttonsPressed[i] != xControl.getRawButton(i)) {
-                    ListenerManager.callListener("button" + XControlMap.getBtnString(i) + (xControl.getRawButton(i) ? "Down" : "Up"));
-                    DebugLog.log(4, referenceName, "Button " + XControlMap.getBtnString(i) + (xControl.getRawButton(i)==true?" pressed.":" released."));
-                }
-                buttonsPressed[i] = xControl.getRawButton(i);
+        for(int i = 1; i < 11; i++) {
+            if(buttonsPressed[i] != xControl.getRawButton(i)) {
+                ListenerManager.callListener("button" + XControlMap.getBtnString(i) + (xControl.getRawButton(i) ? "Down" : "Up"));
+                DebugLog.log(4, referenceName, "Button " + XControlMap.getBtnString(i) + (xControl.getRawButton(i)==true?" pressed.":" released."));
             }
-        } catch(NullPointerException e) {
-            DebugLog.log(1, this.toString(), "Event was executed before controller was bound! Cancelling this event!");
-            this.cancelEvent();
+            buttonsPressed[i] = xControl.getRawButton(i);
         }
     }
 }
