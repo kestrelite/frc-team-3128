3f59773ac38cdba48fe3c1676f30da0867a5c4bc
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-02-18 00:44:48 +0000

    Project cleanup.

--- trunk/FRC-3128-2013/src/frc3128/DriveTank/TurnToAngle.java
+++ trunk/FRC-3128-2013/src/frc3128/DriveTank/TurnToAngle.java
@@ -4,26 +4,35 @@ import frc3128.EventManager.Event;
 import frc3128.Global;
 import frc3128.ListenerManager.ListenerManager;
 
+class StopDrive extends Event {
+    public void execute() {
+        Global.mLB.set(0);
+        Global.mLF.set(0);
+        Global.mRB.set(0);
+        Global.mRF.set(0);        
+    }
+}
+
 public class TurnToAngle extends Event {
-    private final double  thresh      = 1;
-    private       double  targetAngle = -1;
-    
+
+    private final double thresh = 1;
+    private double targetAngle = -1;
+
     public void setTurnAngle(double desire) {
         targetAngle = desire;
         this.registerIterableEvent();
     }
 
     public void execute() {
-        
-        if(1==1) throw new Error("Must set gyro polarization");
-        if(Math.abs(Global.gTurn.getAngle() - this.targetAngle) > thresh) {
+        if (1 == 1) throw new Error("Must set gyro polarization");
             
-            Global.mLB.set(((this.targetAngle - Global.gTurn.getAngle())/90)+.2);
-            Global.mLF.set(((this.targetAngle - Global.gTurn.getAngle())/90)+.2);
-            Global.mRB.set(((this.targetAngle - Global.gTurn.getAngle())/-90)+.2);
-            Global.mRF.set(((this.targetAngle - Global.gTurn.getAngle())/-90)+.2);
-        }
-        else Global.StopDrive();
-        }
+        if (Math.abs(Global.gTurn.getAngle() - this.targetAngle) > thresh) {
+
+            Global.mLB.set(((this.targetAngle - Global.gTurn.getAngle()) / 90) + .2);
+            Global.mLF.set(((this.targetAngle - Global.gTurn.getAngle()) / 90) + .2);
+            Global.mRB.set(((this.targetAngle - Global.gTurn.getAngle()) / -90) + .2);
+            Global.mRF.set(((this.targetAngle - Global.gTurn.getAngle()) / -90) + .2);
+        } else
+            (new StopDrive()).registerSingleEvent();
     }
-   
+}
