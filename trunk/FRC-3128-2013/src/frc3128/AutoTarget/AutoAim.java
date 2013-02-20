08b4ee2eb831fb6a820c579ba4206d9398a2235a
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-02-20 06:03:06 +0000

    AutoAim functional. Frisbee hits target.

--- trunk/FRC-3128-2013/src/frc3128/AutoTarget/AutoAim.java
+++ trunk/FRC-3128-2013/src/frc3128/AutoTarget/AutoAim.java
@@ -3,6 +3,7 @@ package frc3128.AutoTarget;
 import com.sun.squawk.util.MathUtils;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import frc3128.Connection.Connection;
+import frc3128.DebugLog;
 import frc3128.DriveTank.StopDrive;
 import frc3128.EventManager.Event;
 import frc3128.EventManager.EventSequence.EventSequencer;
@@ -13,17 +14,22 @@ import frc3128.PneumaticsManager.PneumaticsManager;
 
 public class AutoAim extends Event {
     EventSequencer aAim;
+    
+    public AutoAim() {
+        this.execute();
+    }
+    
     public void execute() {
         aAim = new EventSequencer();
+        aAim.addEvent(new AutoTurn());
         aAim.addEvent(new SingleSequence() {
             public void execute() {
                 Global.mShoot.set(-1.0);
                 Global.mShoot2.set(-1.0);
             }
         });
-        aAim.addEvent(new AutoTurn());
-        //aAim.addEvent(new AutoLock());
-        //aAim.addEvent(new AutoFire());
+        aAim.addEvent(new ATiltLock());
+        aAim.addEvent(new AutoFire());
         aAim.addEvent(new SingleSequence() {
             public void execute() {
                 Global.mShoot.set(0);
@@ -40,12 +46,16 @@ class AutoTurn extends SequenceEvent {
     
     public void execute() {
         xOff = NetworkTable.getTable("camera").getNumber("xoffset");
+        System.out.println("xOff:"+xOff+", pow:"+Global.mLF.get());
         if (Math.abs(xOff) > thresh) {
-            Global.mLB.set(((-xOff) / 90) - .2);
-            Global.mLF.set(((-xOff) / 90) - .2);
-            Global.mRB.set(((-xOff) / -90) + .2);
-            Global.mRF.set(((-xOff) / -90) + .2);
-        } else (new StopDrive()).registerSingleEvent();
+            Global.mLF.set(-0.95*((xOff) / 75));
+            Global.mRF.set(-0.95*((xOff) / 75));
+        } else {
+            Global.mLF.set(0);
+            Global.mRF.set(0);
+            Global.mRB.set(0);
+            Global.mLB.set(0);
+        }
     }
 
     public boolean exitConditionMet() {
@@ -53,35 +63,41 @@ class AutoTurn extends SequenceEvent {
     }
 }
 
-class AutoLock extends SequenceEvent {
+class ATiltLock extends SequenceEvent {
     private double angle = 0;
-    private double max = -31.0;
-    private double thShift = 5.0;
-    private double thresh = 3.0;
+    private boolean isLocked = false;
+    private boolean taskDone = false;
+    private double max = -35.0;
     
     public void execute() {
-        this.angle = 180*(MathUtils.atan2(92, Connection.distToGoal))/(Math.PI)+thShift;
+        if(!isLocked) {
+            this.angle = -180*(MathUtils.atan2(92, Connection.distToGoal))/(Math.PI)*0.9;
+            if(Connection.distToGoal != 0) isLocked = true;
+            else return;
+        }
         
-        if(this.angle < this.max) this.angle = this.max; if(this.angle > -1) this.angle = 0;
-        if(Math.abs(angle - Global.gTilt.getAngle()) > 1) 
-                Global.mTilt.set(-1.0*(angle - Global.gTilt.getAngle())/40.0+0.1);
-        else Global.mTilt.set(.20);
+        System.out.println("angle: "+angle+" cAng: "+Global.gTilt.getAngle() + " pow:" + Global.mTilt.get());
+        if(this.angle < this.max) {DebugLog.log(1, referenceName, "Tilt targeted to invalid angle! " + this.angle); this.angle = this.max;} 
+        if(this.angle > -1) {DebugLog.log(1, referenceName, "Tilt targeted to invalid angle! " + this.angle); this.angle = 0;}
+        if(isLocked && Math.abs(angle - Global.gTilt.getAngle()) > 1.0) 
+            Global.mTilt.set(-1.0*(angle - Global.gTilt.getAngle())/55.0+0.15);
+        else {if(isLocked) Global.mTilt.set(.15); taskDone = true;}
     }
 
     public boolean exitConditionMet() {
-        return (Math.abs(Global.gTilt.getAngle()-this.angle) < thresh);
+        return taskDone;
     }
 }
 
 class AutoFire extends SequenceEvent {
     public boolean exitConditionMet() {
-        return (this.getRunTimeMillis() > 2000);
+        return (this.getRunTimeMillis() > 2750);
     }
 
     private long lastTime = -1;
     public void execute() {
-        if(lastTime < 750  && this.getRunTimeMillis() >=  750) PneumaticsManager.setPistonInvertState(Global.pstFire);
-        if(lastTime < 1500 && this.getRunTimeMillis() >= 1500) PneumaticsManager.setPistonInvertState(Global.pstFire);
+        if(lastTime < 1250  && this.getRunTimeMillis() >=  1250) PneumaticsManager.setPistonInvertState(Global.pstFire);
+        if(lastTime < 2250 && this.getRunTimeMillis() >= 2250) PneumaticsManager.setPistonInvertState(Global.pstFire);
         lastTime = this.getRunTimeMillis();
     }
 }
