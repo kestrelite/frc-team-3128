7c75794bee9a4cf7fda2bcadafc41294dafc9c51
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-07-30 21:28:55 +0000

    Removed redundant code

--- trunk/robotics-2013/FRC-3128-2014/src/frc3128/EventManager/EventSequence/SingleSequence.java
+++
@@ -1,5 +0,0 @@
-package frc3128.EventManager.EventSequence;
-
-public abstract class SingleSequence extends SequenceEvent {
-    public final boolean exitConditionMet() {return true;}
-}
--- trunk/tmp/temprepo/hooks/pre-revprop-change.bat
+++
@@ -1,4 +0,0 @@
-rem Only allow log messages to be changed.
-if “%4? == “svn:log” exit 0
-echo Property ‘%4' cannot be changed >&2
-exit 1
