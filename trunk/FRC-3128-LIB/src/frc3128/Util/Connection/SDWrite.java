43343c9b119ea1e3b95bfe0bd8c77643717cfb36
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-07-31 05:23:57 +0000

    Moved most of everything else to Util, too

---
+++ trunk/FRC-3128-LIB/src/frc3128/Util/Connection/SDWrite.java
@@ -0,0 +1,41 @@
+package frc3128.Util.Connection;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+/**
+ * 
+ * @author Yousuf Soliman
+ */
+public class SDWrite {
+	/**
+	 * Gets an integer value from the SmartDashboard, if it is open and connected.
+	 * 
+	 * @param table The name of the table to retrieve from
+	 * @param key   The key for the data in the table 
+	 * @return      An integer value from the table if it exists
+	 *              <p>Null if it does not
+	 */
+	public static int getInt(String table, String key) {return (int) NetworkTable.getTable(table).getNumber(key);}
+
+	/**
+	 * Gets a double value from the SmartDashboard, if it is open and connected.
+	 * 
+	 * @param table The name of the table to retrieve from
+	 * @param key   The key for the data in the table 
+	 * @return      A double value from the table if it exists
+	 *              <p>Null if it does not
+	 */	
+	public static double getDouble(String table, String key) {return NetworkTable.getTable(table).getNumber(key);}
+
+	/**
+	 * Gets a double value from the SmartDashboard, if it is open and connected.
+	 * 
+	 * @param table The name of the table to retrieve from
+	 * @param key   The key for the data in the table 
+	 * @return      A double value from the table if it exists
+	 *              <p>Null if it does not
+	 */		
+	public static String getString(String table, String key) {return NetworkTable.getTable(table).getString(key);}
+
+	private SDWrite() {}
+}
---
+++ trunk/FRC-3128-LIB/src/frc3128/Util/PneumaticsManager/PistonID.java
@@ -0,0 +1,29 @@
+package frc3128.Util.PneumaticsManager;
+
+/**
+ * 
+ * @author Noah Sutton-Smolin
+ */
+public class PistonID {
+    private int     value = -1;
+    private boolean inverted = false;
+    
+    protected PistonID(int value) {this.value = value;}
+	
+	/**
+	 * Inverts the piston internally.
+	 */
+    public void invertPiston() {this.inverted = !this.inverted;}
+
+	/**
+	 * 
+	 * @return The indexing ID of the piston
+	 */
+    protected int getID() {return this.value;}
+	
+	/**
+	 * 
+	 * @return Whether or not the current piston is inverted
+	 */
+    protected boolean getInversion() {return this.inverted;}
+}
