715c896790febf5ef099ae49ccb646e2d698921b
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-07-31 04:00:30 +0000

    Javadocced SDRead and SDWrite

--- FRC-3128-LIB/src/frc3128/Connection/SDWrite.java
+++ FRC-3128-LIB/src/frc3128/Connection/SDWrite.java
@@ -3,9 +3,35 @@ package frc3128.Connection;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class SDWrite {
-	private static boolean isConnected = false;
-	
-	public int getInt(String table, String key) {return (int) NetworkTable.getTable(table).getNumber(key);}
-	public double getDouble(String table, String key) {return NetworkTable.getTable(table).getNumber(key);}
-	public String getString(String table, String key) {return NetworkTable.getTable(table).getString(key);}
+	/**
+	 * Gets an integer value from the SmartDashboard, if it is open and connected.
+	 * 
+	 * @param table The name of the table to retrieve from
+	 * @param key   The key for the data in the table 
+	 * @return      An integer value from the table if it exists
+	 *              <p>Null if it does not
+	 */
+	public static int getInt(String table, String key) {return (int) NetworkTable.getTable(table).getNumber(key);}
+
+	/**
+	 * Gets a double value from the SmartDashboard, if it is open and connected.
+	 * 
+	 * @param table The name of the table to retrieve from
+	 * @param key   The key for the data in the table 
+	 * @return      A double value from the table if it exists
+	 *              <p>Null if it does not
+	 */	
+	public static double getDouble(String table, String key) {return NetworkTable.getTable(table).getNumber(key);}
+
+	/**
+	 * Gets a double value from the SmartDashboard, if it is open and connected.
+	 * 
+	 * @param table The name of the table to retrieve from
+	 * @param key   The key for the data in the table 
+	 * @return      A double value from the table if it exists
+	 *              <p>Null if it does not
+	 */		
+	public static String getString(String table, String key) {return NetworkTable.getTable(table).getString(key);}
+
+	private SDWrite() {}
 }
