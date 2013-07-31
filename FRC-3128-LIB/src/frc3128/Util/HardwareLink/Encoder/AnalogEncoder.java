1512ac10538ffa1e4fe648570fbde466a4305ebb
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-07-31 05:56:30 +0000

    Added the Encoder classes

---
+++ FRC-3128-LIB/src/frc3128/Util/HardwareLink/Encoder/AnalogEncoder.java
@@ -0,0 +1,34 @@
+package frc3128.Util.HardwareLink.Encoder;
+
+import edu.wpi.first.wpilibj.AnalogChannel;
+
+/**
+ *
+ * @author Noah Sutton-Smolin
+ */
+public class AnalogEncoder extends AbstractEncoder {
+	private AnalogChannel enc;
+	
+	public AnalogEncoder(int a, int b) {
+		enc = new AnalogChannel(a, b);
+	}
+	
+	/**
+	 * Gets the approximated angle from a magnetic encoder. It uses values which
+	 * have been estimated to high accuracy from extensive tests. Unless need be
+	 * , do not modify these values.
+	 * 
+	 * @return The approximate angle from 0 to 360 degrees of the encoder
+	 */
+	public double getAngle() {
+        double voltage = 0, value = 0;
+        for(char i = 0; i<10; i++) {
+            voltage += enc.getVoltage();
+            value += enc.getValue();
+        }
+        voltage /= 10; value /= 10;
+        return voltage/5.0*360;
+    }
+
+	public double getRawValue() {return enc.getVoltage();}
+}
--- FRC-3128-LIB/src/frc3128/Util/MotorMath.java
+++ FRC-3128-LIB/src/frc3128/Util/MotorMath.java
@@ -1,29 +1,16 @@
 package frc3128.Util;
 
-import edu.wpi.first.wpilibj.AnalogChannel;
-
 /**
  *
  * @author Noah Sutton-Smolin
  */
 public class MotorMath {
-	/**
-	 * Gets the approximated angle from a magnetic encoder. It uses values which
-	 * have been estimated to high accuracy from extensive tests. Unless need be
-	 * , do not modify these values.
-	 * 
-	 * @param channel The analog channel to be read from
-	 * @return The approximate angle from 0 to 360 degrees of the encoder
-	 */
-	public static double getAngleFrom(AnalogChannel channel) {
-        double voltage = 0, value = 0;
-        for(char i = 0; i<10; i++) {
-            voltage += channel.getVoltage();
-            value += channel.getValue();
-        }
-        voltage /= 10; value /= 10;
-        return voltage/5.0*360;
-    }
+	public static double normalizeAngle(double angle) {
+		double out = angle;
+		while(out < 0) out += 360;
+		while(out > 360) out -= 360;
+		return out;
+	}
 	
 	private MotorMath() {}
 }
