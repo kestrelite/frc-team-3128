f9b981049780f81dfec844c5f9e7013400d6ccd8
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-07-31 03:52:02 +0000

    Created the Util package with math and constants.

--- FRC-3128-LIB/src/frc3128/Global.java
+++ FRC-3128-LIB/src/frc3128/Global.java
@@ -1,7 +1,8 @@
 package frc3128;
 
-import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Watchdog;
+import frc3128.EventManager.EventManager;
+import frc3128.EventManager.ListenerManager;
 
 public class Global {
     public static void initializeRobot() {}
@@ -10,18 +11,7 @@ public class Global {
     public static void initializeTeleop() {}
 
 	public static void robotKill() {Watchdog.getInstance().kill();}
-    public static void robotStop() {}
+    public static void robotStop() {EventManager.dropAllEvents(); ListenerManager.dropAllListeners();}
 	
-	//TODO: Move the following functions to their appropriate mathematics libraries
-    public static double getAngleFrom(AnalogChannel c) {
-        double voltage = 0, value = 0;
-        for(char i = 0; i<10; i++) {
-            voltage += c.getVoltage();
-            value += c.getValue();
-        }
-        voltage /= 10; value /= 10;
-        return voltage/5.0*360;
-    }
-
 	private Global() {}
 }
---
+++ FRC-3128-LIB/src/frc3128/Util/Constants.java
@@ -0,0 +1,11 @@
+package frc3128.Util;
+
+/**
+ *
+ * @author Noah
+ */
+
+public class Constants {
+	public static final int DEFAULT_LOGLEVEL = 3;
+	private Constants() {}
+}
