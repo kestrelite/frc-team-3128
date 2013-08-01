258ffefb80c982c54b9ef30a4d9b35e5d9cbb93b
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-08-01 05:03:56 +0000

    Working on implementing AbstractSpeedControl

--- trunk/FRC-3128-LIB/src/frc3128/Util/SpeedControl/AbstractSpeedControl.java
+++ trunk/FRC-3128-LIB/src/frc3128/Util/SpeedControl/AbstractSpeedControl.java
@@ -4,4 +4,34 @@ import frc3128.EventManager.Event;
 
 public abstract class AbstractSpeedControl extends Event {
 	//TODO: Create the abstract methods & a PID implementation
+	private long lastRuntime = -1;
+	public SpeedControlled spdControlTarget = null;
+	
+	public AbstractSpeedControl(SpeedControlled spdControlTarget) {this.spdControlTarget = spdControlTarget;}
+	
+	public abstract void setTarget(double d);
+	public abstract void speedTimestep();
+	
+	/**
+	 * 
+	 * @return the last runtime in system clock milliseconds
+	 */
+	public final long getLastRuntime() {return lastRuntime;}
+	
+	/**
+	 * 
+	 * @return how long ago the event was last called (used for dT)
+	 */
+	public final long getLastRuntimeDist() {return System.currentTimeMillis() - lastRuntime;}
+
+	/**
+	 * 
+	 * @return the object which is the target of speed control
+	 */
+	public final SpeedControlled getSpeedControlledTarget() {return spdControlTarget;}
+	
+	public final void execute() {
+		lastRuntime = System.currentTimeMillis();
+		this.speedTimestep();
+	}
 }
