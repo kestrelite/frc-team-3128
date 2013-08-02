b27e624f794955894b6acffa460aaf2729c9b88d
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-08-02 06:53:35 +0000

    Created a structure for default speed controllers; will fill in settings later.

---
+++ FRC-3128-2014/src/frc3128/HardwareLink/Motor/SpeedControl/LinearSpeedTarget.java
@@ -0,0 +1,16 @@
+package frc3128.HardwareLink.Motor.SpeedControl;
+
+import frc3128.HardwareLink.Motor.MotorSpeedControl;
+
+/**
+ *
+ * @author Noah Sutton-Smolin
+ */
+public class LinearSpeedTarget extends MotorSpeedControl {
+	//TODO: LinearSpeedTarget	
+	public void setControlTarget(double d) {}
+
+	public void speedTimestep(double dt) {}
+
+	public void clearControlRun() {}
+}
---
+++ FRC-3128-2014/src/frc3128/HardwareLink/Motor/SpeedControl/PIDAngleTarget.java
@@ -0,0 +1,16 @@
+package frc3128.HardwareLink.Motor.SpeedControl;
+
+import frc3128.HardwareLink.Motor.MotorSpeedControl;
+
+/**
+ *
+ * @author Noah Sutton-Smolin
+ */
+public class PIDAngleTarget extends MotorSpeedControl {
+	//TODO: PIDAngleTarget
+	public void setControlTarget(double d) {}
+
+	public void speedTimestep(double dt) {}
+
+	public void clearControlRun() {}	
+}
