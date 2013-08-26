00fc21386a693647ad3ef92da6e67d677e56563c
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-08-26 19:56:02 +0000

    First FRC lesson

---
+++ branches/intro/1/FRC-3128-2014/src/frc3128/HardwareLink/Motor/MotorDir.java
@@ -0,0 +1,13 @@
+package frc3128.HardwareLink.Motor;
+
+/**
+ *
+ * @author Noah Sutton-Smolin
+ */
+public class MotorDir {
+	public static final int CW = 1;
+	public static final int EITHER = 0;
+	public static final int CCW = -1;
+	
+	private MotorDir() {}
+}
