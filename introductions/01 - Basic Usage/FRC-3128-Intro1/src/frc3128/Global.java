2ed0c8482eecda378f5d6f1776f0032dc6d8ae02
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-08-29 05:20:53 +0000

    Finished the Basic Usage tutorial

--- introductions/01 - Basic Usage/FRC-3128-Intro1/src/frc3128/Global.java
+++ introductions/01 - Basic Usage/FRC-3128-Intro1/src/frc3128/Global.java
@@ -1,13 +1,7 @@
 package frc3128;
 
 import edu.wpi.first.wpilibj.Watchdog;
-import frc3128.EventManager.EventManager;
-import frc3128.EventManager.ListenerManager;
 
-/**
- * 
- * @author Noah Sutton-Smolin
- */
 public class Global {
     public static void initializeRobot() {}
     public static void initializeDisabled() {}
@@ -15,7 +9,7 @@ public class Global {
     public static void initializeTeleop() {}
 
     public static void robotKill() {Watchdog.getInstance().kill();}
-    public static void robotStop() {EventManager.dropAllEvents(); ListenerManager.dropAllListeners();}
+    public static void robotStop() {Watchdog.getInstance().kill();}
 
     private Global() {}
 }
