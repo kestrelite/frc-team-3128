007261541c1929ba8b4e8bd048c0fda08b2d134a
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-09-03 05:21:27 +0000

    Replacing /introductions with /intro

---
+++ intro/03 - Event Manager/FRC-3128-Intro3/src/frc3128/EventManager/EventSequence/SingleSequence.java
@@ -0,0 +1,14 @@
+package frc3128.EventManager.EventSequence;
+
+/**
+ * 
+ * @author Noah Sutton-Smolin
+ */
+public abstract class SingleSequence extends SequenceEvent {
+	/**
+	 * Exits immediately; will always return true.
+	 * 
+	 * @return true
+	 */
+    public final boolean exitConditionMet() {return true;}
+}
---
+++ intro/03 - Event Manager/FRC-3128-Intro3/src/frc3128/HardwareLink/Encoder/GyroEncoder.java
@@ -0,0 +1,23 @@
+package frc3128.HardwareLink.Encoder;
+
+import edu.wpi.first.wpilibj.Gyro;
+import frc3128.HardwareLink.GyroLink;
+
+/**
+ *
+ * @author Noah Sutton-Smolin
+ */
+//TODO Test GyroEncoder
+public class GyroEncoder extends AbstractEncoder {
+	private GyroLink gyr;
+	
+	/**
+	 * Creates a gyroscope encoder.
+	 * 
+	 * @param gyr The gyroscope acting as an encoder.
+	 */
+	public GyroEncoder(Gyro gyr) {this.gyr = new GyroLink(gyr);}
+	
+	public double getAngle() {return gyr.getAngle();}
+	public double getRawValue() {return -1;}
+}
