512db9653919bc883070501405f2c58900032123
noahsutsmo@gmail.com <noahsutsmo@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-09-29 01:09:33 +0000

    Added a ResourceLock for locking synchronized resources. Deleted DualSolenoid; moved it into PneumaticsLink.

--- trunk/FRC-3128-2014/src/frc3128/HardwareLink/Pneumatics/PneumaticsLink.java
+++ trunk/FRC-3128-2014/src/frc3128/HardwareLink/Pneumatics/PneumaticsLink.java
@@ -2,6 +2,7 @@ package frc3128.HardwareLink.Pneumatics;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
+import frc3128.Util.DebugLog;
 import java.util.Vector;
 
 /**
@@ -22,6 +23,16 @@ public class PneumaticsLink {
         PneumaticsLink.c.stop();
     }
     
+    /**
+     * Sets the compressor. Do not call this unless needed.
+     *
+     * @param comp The compressor to be used
+     */
+    public void setCompressor(Compressor comp) {
+        if(PneumaticsLink.c != null) DebugLog.log(DebugLog.LVL_SEVERE, "PneumaticsLink", "Pneumatic compressor was changed!");
+        PneumaticsLink.c = comp;
+        PneumaticsLink.c.stop();
+    }
     /**
      * Adds a piston to the DualSolenoid list
      * 
@@ -84,7 +95,7 @@ public class PneumaticsLink {
      * @param id the PistonID to be inverted
      */
     public static void setPistonInvertState(PistonID id) {
-        ((DualSolenoid) dualSolenoidList.elementAt(id.getID())).setPistonInverted();
+        ((DualSolenoid) dualSolenoidList.elementAt(id.getID())).setPistonInvertState();
     }
     
     /**
@@ -111,5 +122,71 @@ public class PneumaticsLink {
     public static void lockAllPistons() {
         for(int i = 0; i < dualSolenoidList.size(); i++)
             ((DualSolenoid) dualSolenoidList.elementAt(i)).lockPiston();
+    }
+}
+
+class DualSolenoid {
+    private Solenoid solA;
+    private Solenoid solB;
+
+    /**
+     * Links two solenoids together, typically a Festo valve.
+     * 
+     * @param solA      the first solenoid valve
+     * @param solB      the second solenoid valve
+     * @param solAState the initial solenoid state for the first valve
+     * @param solBState the initial solenoid state for the second valve
+     */
+    protected DualSolenoid(Solenoid solA, Solenoid solB, boolean solAState, boolean solBState) {
+        this.solA = solA;
+        this.solB = solB;
+        solA.set(solAState);
+        solB.set(solBState);
+    }
+
+    /**
+     * Sets both solenoid states to "true"; locks the piston in place.
+     */
+    protected void lockPiston() {
+        solA.set(true);
+        solB.set(true);
+        DebugLog.log(DebugLog.LVL_STREAM, "DualSolenoid", "DualSolenoid set to lock-state");
+    }
+
+    /**
+     * Sets both solenoid states to "false"; also typically locks the piston in 
+     * place.
+     */
+    protected void unlockPiston() {
+        solA.set(false);
+        solB.set(false);
+        DebugLog.log(DebugLog.LVL_STREAM, "DualSolenoid", "DualSolenoid set to unlock-state");
+    }
+
+    /**
+     * Sets A to true and B to false; generically called "on".
+     */
+    protected void setPistonOn() {
+        solA.set(true);
+        solB.set(false);
+        DebugLog.log(DebugLog.LVL_STREAM, "DualSolenoid", "DualSolenoid set to on-state");
+    }
+
+    /**
+     * Sets B to true and A to false; generically called "off".
+     */
+    protected void setPistonOff() {
+        solB.set(true);
+        solA.set(false);
+        DebugLog.log(DebugLog.LVL_STREAM, "DualSolenoid", "DualSolenoid set to off-state");
+    }
+    
+    /**
+     * Sets B to !B and A to !A, inverting the piston's current state.
+     */
+    protected void setPistonInvertState() {
+        solA.set(!solA.get());
+        solB.set(!solB.get());
+        DebugLog.log(DebugLog.LVL_STREAM, "DualSolenoid", "DualSolenoid set to flip-state " + solA.get() + ", " +solB.get());
     }
 }
