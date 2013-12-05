62f34bd20c570a17ef03e6c9e02a3187fc7327c8
jamiejamaica@gmail.com <jamiejamaica@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2013-12-05 22:36:21 +0000

    Whoops. Add new files from last commit.

---
+++ libSOS/server/src/Options.h
@@ -0,0 +1,33 @@
+/*
+ * This class holds tions for the program.  They are initialized from init_program_options in
+ * main.cpp, and can be changed at runtime.
+ *
+ *  Created on: Dec 3, 2013
+ *      Author: jamie
+ */
+
+#ifndef OPTIONS_H_
+#define OPTIONS_H_
+
+class Options
+{
+private:
+	Options();
+
+    Options(Options const&);              // Don't Implement
+    void operator=(Options const&); // Don't implement
+
+public:
+
+	int _port;
+
+	bool _fake;
+
+	bool _verbose;
+
+	static Options & instance();
+
+
+};
+
+#endif /* OPTIONS_H_ */
