5b42d313a179324a55eeaa5691ac8af49f04147d
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2014-01-01 17:44:49 +0000

    Added degrees to radians to RobotMath. Swerve Drive code is done except for powering motors. The math is done.

---
+++ FRC-3128-2014/src/frc3128/Drive/SwerveDrive.java
@@ -0,0 +1,52 @@
+package frc3128.Drive;
+
+import com.sun.squawk.util.MathUtils;
+import frc3128.EventManager.Event;
+import frc3128.Util.RobotMath;
+
+/**
+ * @author Yousuf M. Soliman
+ */
+class DriveControl extends Event {
+    double angle = 0, speed = 0, rot = 0;
+    double dTx = 0, dTy = 0, dTheta = 0;
+    double dXLF = 0, dXLB = 0, dXRF = 0, dXRB = 0, dYLF = 0, dYLB = 0, dYRF = 0, dYRB = 0;
+    double dLF = 0, dLB = 0, dRF = 0, dRB = 0;
+    double tLF = 0, tLB = 0, tRF = 0, tRB = 0;
+    
+    public void execute() {
+        dTx = speed * Math.cos(RobotMath.dTR(angle));
+        dTy = speed * Math.sin(RobotMath.dTR(angle));
+
+        dTheta = rot;
+
+        dXLF = dTx - rot;
+        dYLF = dTy - dTheta;
+        dXLB = dTx + rot;
+        dYLB = dTy - dTheta;
+        dXRF = dTx - rot;
+        dYRF = dTy + dTheta;
+        dXRB = dTx + rot;
+        dYRB = dTy + dTheta;
+
+        dLF = Math.sqrt(MathUtils.pow(dXLF, 2) + MathUtils.pow(dYLF, 2));
+        dLB = Math.sqrt(MathUtils.pow(dXLB, 2) + MathUtils.pow(dYLB, 2));
+        dRF = Math.sqrt(MathUtils.pow(dXRF, 2) + MathUtils.pow(dYRF, 2));
+        dRB = Math.sqrt(MathUtils.pow(dXRB, 2) + MathUtils.pow(dYRB, 2));
+
+        if (dYLF != 0) tLF = MathUtils.atan2(dYLF, dXLF);
+        else           tLF = 0;
+        if (dYLB != 0) tLB = MathUtils.atan2(dYLB, dXLB);
+        else           tLB = 0;
+        if (dYRF != 0) tRF = MathUtils.atan2(dYRF, dXRF);
+        else           tRF = 0;
+        if (dYRB != 0) tRB = MathUtils.atan2(dYRB, dXRB);
+        else           tRB = 0;
+    }
+}
+
+public class SwerveDrive {
+
+    public SwerveDrive() {
+    }
+}
