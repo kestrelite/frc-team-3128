89949dade6552a3a372075fdccd1b1c12832b1c9
jamiejamaica@gmail.com <jamiejamaica@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2014-01-17 01:16:33 +0000

    new version of RobotCommand parser

--- FRC-3128-2014/src/frc3128/Util/Connection/Beaglebone.java
+++ FRC-3128-2014/src/frc3128/Util/Connection/Beaglebone.java
@@ -3,58 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package frc3128.Util.Connection;
 
-import edu.wpi.first.wpilibj.DigitalModule;
-import edu.wpi.first.wpilibj.SPIDevice;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.fpga.tSPI;
+import javax.microedition.io.ServerSocketConnection;
 import frc3128.EventManager.Event;
 import frc3128.Util.DebugLog;
 
-/**
- * Connects over SPI with the Beaglebone
+/**Connects over SPI with the Beaglebone
  *
  * @author jamie
  */
-public class Beaglebone extends Event {
-
-    /*
-     BEAGLEBONE:
-     P9_22: SCLK P9_18: DI P9_21: DO P9_17: CS0
+public class Beaglebone extends Event
+{
+    ServerSocketConnection socketServer;
     
-     DIGITAL SIDECAR:
-     GPIO1: MOSI GPIO2: MISO GPIO3: SCLK GPIO4: CS
-     */
-    //YESSSSSSS
-    //found some documentation at http://www.chiefdelphi.com/forums/showthread.php?p=1081779
-    SPIDevice spi;
-    DigitalModule module;
-    int slot;
-    final static int clockPin = 3;
-    final static int dataOutPin = 2;
-    final static int dataInPin = 1;
-    final static int chipSelectPin = 4;
-
-    public Beaglebone(int slotParam) {
-        slot = slotParam;
-        spi = new SPIDevice(slot, clockPin, dataOutPin, dataInPin, chipSelectPin);
-    }
-
+    public Beaglebone()
+    {
+        socketServer = new ServerSocketConnection();
+    } 
+    
     /**
-     * gets one byte from SPI (MOSI)
-     *
-     * @return
+     * 
+     * 
+     * @return 
      */
-    long readSPI() {
+    byte[] readCommand()
+    {
         // read the data
         long rawData = 0xA;
-        spi.transfer(rawData, 8);
+        
+        spi.transfer(rawData, 1);
+        
         return rawData;
     }
 
-    public void execute() {
+    public void execute()
+    {
+        
         long data = readSPI();
-        DebugLog.log(DebugLog.LVL_DEBUG, this, "SPI read " + Long.toString(data, 16));
+        DebugLog.log(DebugLog.LVL_INFO, this, "SPI read " + Long.toString(data, 16));
     }
+    
+    
+
+    
 }
