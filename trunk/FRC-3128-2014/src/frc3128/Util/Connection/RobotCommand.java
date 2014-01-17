b42673e34dc6b6c25b267cdaf754df25aab1d096
jamiejamaica@gmail.com <jamiejamaica@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2014-01-17 01:16:33 +0000

    new version of RobotCommand parser

--- trunk/FRC-3128-2014/src/frc3128/Util/Connection/RobotCommand.java
+++ trunk/FRC-3128-2014/src/frc3128/Util/Connection/RobotCommand.java
@@ -15,46 +15,53 @@ import java.util.Vector;
  */
 public class RobotCommand 
 {
+    //this is an unsigned int in the c++ program
+    //but we need to round up here
+    //java, folks
+    int _return_id;
+    
     byte _opcode;
     
     Vector _shorts;
     
     String _extraString;
     
-    public RobotCommand(byte opcode)
+    public RobotCommand(int return_id, byte opcode)
     {
         _opcode = opcode;
     }
     
-    public RobotCommand(byte opcode, Vector shorts)
+    public RobotCommand(int return_id, byte opcode, Vector shorts)
     {
         _opcode = opcode;
-        
+
         _shorts = shorts;
     }
-    
-    public RobotCommand(byte opcode, Vector shorts, String extraString)
+
+    public RobotCommand(int return_id, byte opcode, Vector shorts, String extraString)
     {
         _opcode = opcode;
-        
+
         _shorts = shorts;
-        
+
         _extraString = extraString;
     }
-    
-    public RobotCommand(byte opcode, String extraString)
+
+    public RobotCommand(int return_id, byte opcode, String extraString)
     {
         _opcode = opcode;
-        
+
         _extraString = extraString;
     }
-    
+
     public String toString()
     {
         String string = "Robot Command dump:---------------\n";
         
+        string += "Return ID: " + _return_id + "\n";
+
         string += "Opcode: " + Integer.toHexString(_opcode) + "\n";
-        
+
         //print out each short
         if(_shorts != null)
         {
@@ -65,12 +72,12 @@ public class RobotCommand
             }
             string += "\n";
         }
-        
+
         string += "Extra String: " + _extraString + "\n";
         return string;
     }
-    
-    //Parsing code originally written in c++, where it was a lot... better
+
+        //Parsing code originally written in c++, where it was a lot... better
     
     public static RobotCommand factory(byte[] bytes)
     {
@@ -81,43 +88,72 @@ public class RobotCommand
             currentBytePtr.addElement(Byte.valueOf(bytes[counter]));
         }
         
+        return factory(currentBytePtr);
+    }
+
+    public static RobotCommand factory(Vector currentBytePtr)
+    {
+        int return_id = parseReturnID(currentBytePtr);
+        
+        ++iterator;
+        
         byte opcode = parseOpcode(currentBytePtr);
         Vector shorts = parseShorts(currentBytePtr);
         String extraString = parseString(currentBytePtr);
-        
+
         iterator = 0;
-        return new RobotCommand(opcode, shorts, extraString);
+        return new RobotCommand(return_id, opcode, shorts, extraString);
+    }
+
+        //needs to be class scope so that it can keep its state after a function call
+        static int iterator = 0;
+
+    static byte parseOpcode(Vector currentBytePtr)
+    {
+        // Get opcode.
+        ++iterator;
+        byte opcode = ((Byte)currentBytePtr.elementAt(iterator)).byteValue();
+        ++iterator;
+
+        if(((Byte)currentBytePtr.elementAt(iterator)).byteValue() != SOSProtocol.END_OPCODE)
+        {
+                // Bail and return empty value.
+                DebugLog.log(DebugLog.LVL_ERROR, null, "RobotCommand: the end of the opcode command is incorrect\nShould be:" + SOSProtocol.END_OPCODE + " Was: " + ((Byte)currentBytePtr.elementAt(iterator)).byteValue());
+                return 0x0;
+        }
+
+        return opcode;
     }
     
-    //needs to be class scope so that it can keep its state after a function call
-    static int iterator = 0;
-    
-    static byte parseOpcode(Vector currentBytePtr)
-{
-	if(((Byte)currentBytePtr.elementAt(iterator)).byteValue() != SOSProtocol.START_TRANSMISSION)
-	{
-		// Bail and return empty value.
+    static int parseReturnID(Vector currentBytePtr)
+    {
+        if(((Byte)currentBytePtr.elementAt(iterator)).byteValue() != SOSProtocol.START_TRANSMISSION)
+        {
+                // Bail and return empty value.
             DebugLog.log(DebugLog.LVL_ERROR, null, "RobotCommand: the header of the command is incorrect\nShould be:" + SOSProtocol.START_TRANSMISSION + " Was: " + currentBytePtr.elementAt(iterator));
-	    return 0x0;
-	}
-
+            return 0x0;
+        }
 
 	// Have start of command.  Begin parsing.
 
-	// Get opcode.
-	++iterator;
-	byte opcode = ((Byte)currentBytePtr.elementAt(iterator)).byteValue();
-	++iterator;
 
-	if(((Byte)currentBytePtr.elementAt(iterator)).byteValue() != SOSProtocol.END_OPCODE)
-	{
-		// Bail and return empty value.
-		DebugLog.log(DebugLog.LVL_ERROR, null, "RobotCommand: the end of the opcode command is incorrect\nShould be:" + SOSProtocol.END_OPCODE + " Was: " + ((Byte)currentBytePtr.elementAt(iterator)).byteValue());
-		return 0x0;
-	}
+	++iterator;
+	//should now equal START_ID
+	++iterator;
+	//should now be the first byte of the ASCII-encoded id
 
-	return opcode;
-}
+        //max of 10 ASCII digit short
+        char[] returnBytesStorage = new char[10];
+        int returnBytesIterator = 0;
+
+        while(((Byte)currentBytePtr.elementAt(++iterator)).byteValue() != SOSProtocol.END_SHORT)
+        {
+                returnBytesStorage[returnBytesIterator] = (char)(((Byte)currentBytePtr.elementAt(iterator)).byteValue());
+                ++returnBytesIterator;
+        }
+
+        return Integer.valueOf(String.valueOf(returnBytesStorage)).intValue();
+    }
 
 static Vector parseShorts(Vector currentBytePtr)
 {
