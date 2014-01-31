9952f1e2410dc10625fd573f4b025a5900191bef
jamiejamaica@gmail.com <jamiejamaica@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2014-01-31 23:21:20 +0000

    add Beaglebone control code to crio

--- trunk/FRC-3128-2014/src/frc3128/Util/Connection/Beaglebone.java
+++ trunk/FRC-3128-2014/src/frc3128/Util/Connection/Beaglebone.java
@@ -6,9 +6,15 @@
 
 package frc3128.Util.Connection;
 
-import javax.microedition.io.ServerSocketConnection;
+import com.sun.squawk.util.Arrays;
 import frc3128.EventManager.Event;
 import frc3128.Util.DebugLog;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.microedition.io.Connector;
+import javax.microedition.io.SocketConnection;
+
 
 /**Connects over SPI with the Beaglebone
  *
@@ -16,33 +22,90 @@ import frc3128.Util.DebugLog;
  */
 public class Beaglebone extends Event
 {
-    ServerSocketConnection socketServer;
+    private static final String BBURL = "socket://10.31.28.144:4545";
+    
+    private byte[] commandInProgress;
+    private short placeInCommandInProgress;
+
+    private SocketConnection socket;
+    private InputStream inStream;
+    private OutputStream outStream;
     
     public Beaglebone()
     {
-        socketServer = new ServerSocketConnection();
+        //hard 200 byte limit on command size
+        commandInProgress = new byte[200];
+        
+        placeInCommandInProgress = -1;
+        
+        try 
+        {
+            DebugLog.log(DebugLog.LVL_INFO, this, "Connecting to " + BBURL);
+            connect();
+        }
+        catch (IOException ex) 
+        {
+            ex.printStackTrace();
+            DebugLog.log(DebugLog.LVL_SEVERE, this , "Error connecting: " + ex.getMessage());
+        }
+        DebugLog.log(DebugLog.LVL_INFO, this, "Connected to beaglebone.");
     } 
     
+    public synchronized void connect() throws IOException 
+    {
+        socket = (SocketConnection) Connector.open(BBURL);//, Connector.READ_WRITE, true);
+        inStream = socket.openInputStream();
+        outStream = socket.openOutputStream();
+    }
+    
     /**
-     * 
-     * 
+     * read bytes from serial port into commandInProgress
+     * stops if it finds an SOSProtocol.END_TRANSMISSION
      * @return 
      */
-    byte[] readCommand()
+    void readCommandBytes()
     {
-        // read the data
-        long rawData = 0xA;
-        
-        spi.transfer(rawData, 1);
-        
-        return rawData;
-    }
+            try 
+            {
+                if(inStream.available() > 0)
+                {
+                    int data = inStream.read();
+                    
+                    //check for EOF
+                    if(data == -1)
+                    {
+                        DebugLog.log(DebugLog.LVL_INFO, this, "Got EOF.");
+                        return;
+                    }
+                    commandInProgress[placeInCommandInProgress + 1] = (byte)data;
+                    //DebugLog.log(DebugLog.LVL_INFO, this, "Beaglebone read byte" + Integer.toHexString(data) + " into slot " + (placeInCommandInProgress + 1));
+                    
+                    ++placeInCommandInProgress;
+                    
+                }
+            }
+            catch (IOException ex) 
+            {
+                ex.printStackTrace();
+            }
+        }
 
     public void execute()
     {
+
+        readCommandBytes();
         
-        long data = readSPI();
-        DebugLog.log(DebugLog.LVL_INFO, this, "SPI read " + Long.toString(data, 16));
+        //if there's one complete command in storage, parse it and use it
+        if((placeInCommandInProgress > -1) && (commandInProgress[placeInCommandInProgress] == SOSProtocol.END_TRANSMISSION))
+        {
+            
+            //RobotCommand command = RobotCommand.factory(commandInProgress);
+            //DebugLog.log(DebugLog.LVL_INFO, this, "Beaglebone read " + command.toString());
+            
+            placeInCommandInProgress = 0;
+            
+            DebugLog.log(DebugLog.LVL_INFO, this, "Done parsing.");
+        }
     }
     
     
--- trunk/FRC-3128-2014/src/frc3128/Util/Connection/SOSProtocol.java
+++ trunk/FRC-3128-2014/src/frc3128/Util/Connection/SOSProtocol.java
@@ -13,6 +13,9 @@ package frc3128.Util.Connection;
 public class SOSProtocol 
 {
     static char START_TRANSMISSION = 0x01;
+    static char START_ID = 0x05;
+    static char END_ID = 0x0A;
+    static char START_OPCODE = 0x0D;
     static char END_OPCODE = 0x04;
     static char START_SHORT_TRANSMISSION = 0x09;
     static char END_SHORT = 0x0B;
