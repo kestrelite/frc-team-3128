83e519d844a489f4a4581b4c97574899927f02f1
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2014-02-01 23:01:42 +0000

    Beaglebone intefacing with the cRIO works now. Beware of Opcode 39, it does not work.

--- trunk/FRC-3128-2014/src/frc3128/Global.java
+++ trunk/FRC-3128-2014/src/frc3128/Global.java
@@ -1,35 +1,67 @@
 package frc3128;
 
 import edu.wpi.first.wpilibj.Watchdog;
+import frc3128.EventManager.Event;
 import frc3128.EventManager.EventManager;
 import frc3128.EventManager.ListenerManager;
+import frc3128.HardwareLink.Controller.RbtCommandController;
 
 import frc3128.Util.Connection.Beaglebone;
 import frc3128.Util.DebugLog;
 
 public class Global {
-
+    public static RbtCommandController rCont;
     public static void initializeRobot() {
         DebugLog.setLogLevel(DebugLog.LVL_DEBUG);
+        rCont = new RbtCommandController();
     }
 
     public static void initializeDisabled() {}
-
     public static void initializeAuto() {}
-
     public static void initializeTeleop() {
-        Beaglebone beaglebone = new Beaglebone();
-        beaglebone.registerIterableEvent();
+        ListenerManager.addListener(new Event() {
+            public void execute() {
+                DebugLog.log(DebugLog.LVL_DEBUG, "Global", "RBT CMD 38");
+            }
+        }, "rbtCmd-38");
+        ListenerManager.addListener(new Event() {
+            public void execute() {
+                DebugLog.log(DebugLog.LVL_DEBUG, "Global", "RBT CMD 39");
+            }
+        }, "rbtCmd-39");
+        ListenerManager.addListener(new Event() {
+            public void execute() {
+                DebugLog.log(DebugLog.LVL_DEBUG, "Global", "RBT CMD 40");
+            }
+        }, "rbtCmd-40");
+        ListenerManager.addListener(new Event() {
+            public void execute() {
+                DebugLog.log(DebugLog.LVL_DEBUG, "Global", "RBT CMD 41");
+            }
+        }, "rbtCmd-41");
+        ListenerManager.addListener(new Event() {
+            public void execute() {
+                DebugLog.log(DebugLog.LVL_DEBUG, "Global", "RBT CMD 42");
+            }
+        }, "rbtCmd-42");
+        ListenerManager.addListener(new Event() {
+            public void execute() {
+                DebugLog.log(DebugLog.LVL_DEBUG, "Global", "RBT CMD 43");
+            }
+        }, "rbtCmd-43");
+        ListenerManager.addListener(new Event() {
+            public void execute() {
+                DebugLog.log(DebugLog.LVL_DEBUG, "Global", "RBT CMD 44");
+            }
+        }, "rbtCmd-44");
     }
 
     public static void robotKill() {
-        Watchdog.getInstance().kill();
+        Watchdog.getInstance().kill(); 
     }
-
     public static void robotStop() {
         EventManager.dropAllEvents();
         ListenerManager.dropAllListeners();
     }
-
     private Global() {}
 }
