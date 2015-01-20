35f43cf39400675f09da782f02664c4d21d0eb33
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@c809eff0-5b5c-19d7-7268-60ae4ad93e4a>
2015-01-20 00:37:51 +0000

    Final frantic competition changea

--- trunk/FRC-3128-2014/src/frc3128/RobotMovement/AutoConfig.java
+++ trunk/FRC-3128-2014/src/frc3128/RobotMovement/AutoConfig.java
@@ -4,31 +4,18 @@ import frc3128.EventManager.Event;
 import frc3128.EventManager.EventSequence.EventSequencer;
 import frc3128.EventManager.EventSequence.SequenceEvent;
 import frc3128.EventManager.EventSequence.SingleSequence;
-import frc3128.EventManager.EventSequence.DelaySequence;
 import frc3128.Global;
-import frc3128.Util.DebugLog;
+import frc3128.Util.Constants;
 
 public class AutoConfig {
     public static void initialize() {
         EventSequencer es = new EventSequencer();
         
-        es.addEvent(new InitSwerveDrive()); //Initialize Swerve Drive Wheel Orientation
-        es.addEvent(new SingleSequence() { //Stop Turning Motors
-            public void execute() {
-                Global.rotBk.setSpeed(0);
-                Global.rotFL.setSpeed(0);
-                Global.rotFR.setSpeed(0);
-            }
-        });
         es.addEvent(new SequenceEvent() { //Drive forward to shooting range
-            public boolean exitConditionMet() {return this.getRunTimeMillis() > 2500;}
+            public boolean exitConditionMet() {return this.getRunTimeMillis() > 2800;}
             public void execute() {
-                Global.rotBk.setControlTarget(90);
-                Global.rotFL.setControlTarget(90);
-                Global.rotFR.setControlTarget(90);
-                
                 Global.drvBk.setSpeed(-0.50);
-                Global.drvFL.setSpeed(0.50);
+                Global.drvFL.setSpeed(-0.50);
                 Global.drvFR.setSpeed(-0.50);
             }
         });
@@ -39,18 +26,20 @@ public class AutoConfig {
                 Global.drvFR.setSpeed(0);
             }
         });
-        es.addEvent(new SequenceEvent() { //Wait if the goal is not hot
-            public boolean exitConditionMet() {return this.getRunTimeMillis() > 5000 || AutoConfig.isHot();}
-            public void execute() {}
-        });
 
-        es.addEvent(new SequenceEvent() { //Run arm until launched
-            public boolean exitConditionMet() {return !Global.shooterTSensor.get();}
-            public void execute() {if(Global.ballTSensor0.get() || Global.ballTSensor1.get()) {Global.mShooter.setSpeed(-1.0);}}
-        });
-        
-        es.addEvent(new DelaySequence(1000));
-        
+        /*es.addEvent(new SequenceEvent() {
+            public boolean exitConditionMet() {
+                return (Vision.targetRecognition() || this.getRunTimeMillis() > 3000);
+            }
+            public void execute() {}
+        });*/
+
+//        if(Constants.SHOOTER_ENABLED)
+//            es.addEvent(new SequenceEvent() { //Run arm until launched
+//                public boolean exitConditionMet() {return !Global.shooterTSensor.get();}
+//                public void execute() {Global.mShooter.setSpeed(-1.0);}
+//            });
+//        
         es.addEvent(new SingleSequence() { //Do a dance
             public void execute() {
                 new LightsFlashEvent(Global.blueLights, true, 1250).registerIterableEvent();
@@ -61,15 +50,9 @@ public class AutoConfig {
         Global.mArmMove.setSpeed(-0.45);
         (new Event() {
             public void execute() {Global.mArmMove.setSpeed(0);}
-        }).registerTimedEvent(300);
-        
+        }).registerTimedEvent(300);   
         es.startSequencer();
     }
-    
-    public static boolean isHot() {
-        DebugLog.log(DebugLog.LVL_SEVERE, "AutoConfig", "isHot event missing!");
-        return true;
-    }
-    
+
     private AutoConfig() {}
 }
--- trunk/FRC-3128-2014/src/frc3128/RobotMovement/SwerveDrive.java
+++ trunk/FRC-3128-2014/src/frc3128/RobotMovement/SwerveDrive.java
@@ -6,13 +6,10 @@ import frc3128.Global;
 import frc3128.Util.RobotMath;
 
 public class SwerveDrive extends Event {
-    //private final double dimConst = 10;
-    //private final double xPosL = -12.77374 / dimConst, xPosR = 12.77374 / dimConst, xPosB = 0.0 / dimConst;
-    //private final double yPosL = 7.375 / dimConst, yPosR = 7.375 / dimConst, yPosB = -14.75 / dimConst;
     private double vel, theta, rot, xVel, yVel;
     private double spdL, spdR, spdB;
     private double angL, angR, angB;
-    private double maxVel = 8, maxRot = RobotMath.dTR(345);
+    private double maxVel = 8, maxRot = RobotMath.dTR(345), c = 0.3;
     private final double xPosL = -12.77374, xPosR = 12.77374, xPosB = 0.0;
     private final double yPosL = 7.375, yPosR = 7.375, yPosB = -14.75;
 
@@ -37,24 +34,24 @@ public class SwerveDrive extends Event {
         double x2 = Math.abs(Global.xControl.x2) > thresh ? Global.xControl.x2 : 0.0;
 
         vel = -(Math.sqrt(MathUtils.pow(x1, 2) + MathUtils.pow(y1, 2)));
-        rot = maxRot * x2;
+        rot = maxRot * x2 * 0.3;
 
         if (Math.abs(vel) > 0.1)
-            theta = RobotMath.rTD(MathUtils.atan2(y1, x1)) + Global.gyr.getAngle();
+            theta = RobotMath.rTD(MathUtils.atan2(y1, x1)) + Global.gyr.getAngle() + Global.gyrBias;
         else 
             vel = 0;
 
         xVel = maxVel * vel * Math.cos(RobotMath.dTR(theta));
         yVel = maxVel * vel * Math.sin(RobotMath.dTR(theta));
 
-        spdL = Math.sqrt(MathUtils.pow(xVel + (rot * yPosL), 2) + MathUtils.pow(yVel - (rot * xPosL), 2));
-        angL = RobotMath.rTD(MathUtils.atan2(yVel + (rot * xPosL), xVel - (rot * yPosL)));
+        spdL = Math.sqrt(MathUtils.pow(xVel + (c*rot * yPosL), 2) + MathUtils.pow(yVel - (c*rot * xPosL), 2));
+        angL = RobotMath.rTD(MathUtils.atan2(yVel + (c*rot * xPosL), xVel - (c*rot * yPosL)));
 
-        spdB = Math.sqrt(MathUtils.pow(xVel + (rot * yPosB), 2) + MathUtils.pow(yVel - (rot * xPosB), 2));
-        angB = 180.0 / Math.PI * (MathUtils.atan2(yVel + (rot * xPosB), xVel - (rot * yPosB)));
+        spdB = Math.sqrt(MathUtils.pow(xVel + (c*rot * yPosB), 2) + MathUtils.pow(yVel - (c*rot * xPosB), 2));
+        angB = 180.0 / Math.PI * (MathUtils.atan2(yVel + (c*rot * xPosB), xVel - (c*rot * yPosB)));
 
-        spdR = Math.sqrt(MathUtils.pow(xVel + (rot * yPosR), 2) + MathUtils.pow(yVel - (rot * xPosR), 2));
-        angR = 180.0 / Math.PI * (MathUtils.atan2(yVel + (rot * xPosR), xVel - (rot * yPosR)));
+        spdR = Math.sqrt(MathUtils.pow(xVel + (c*rot * yPosR), 2) + MathUtils.pow(yVel - (c*rot * xPosR), 2));
+        angR = 180.0 / Math.PI * (MathUtils.atan2(yVel + (c*rot * xPosR), xVel - (c*rot * yPosR)));
 
         double[] r = optimizeSwerve(Global.rotFR.getEncoderAngle(), angR, spdR);
         double[] l = optimizeSwerve(Global.rotFL.getEncoderAngle(), angL, spdL);
@@ -63,7 +60,7 @@ public class SwerveDrive extends Event {
         Global.rotFR.setControlTarget(r[0]+(x1 == 0 && x2 != 0 ? 0.1 : 0));
         Global.rotFL.setControlTarget(l[0]+(x1 == 0 && x2 != 0 ? 0.1 : 0));
         Global.rotBk.setControlTarget(b[0]+(x1 == 0 && x2 != 0 ? 0.1 : 0));
-
+        
         r[1] /= maxVel;
         l[1] /= maxVel;
         b[1] /= maxVel;
@@ -74,9 +71,10 @@ public class SwerveDrive extends Event {
             l[1] /= scl;
             b[1] /= scl;
         }
-        Global.drvFR.setSpeed(r[1]);
-        Global.drvFL.setSpeed(l[1]);
-        Global.drvBk.setSpeed(b[1]);
+        
+        Global.drvFR.setSpeed(-r[1]/1.0);
+        Global.drvFL.setSpeed(-l[1]/1.0);
+        Global.drvBk.setSpeed(-b[1]/1.0);
     }
 
     public SwerveDrive() {}   
